1accc81fbd02e5eec292827ded6ef2f3ea7686a7
Andrei Gheorghe <andrei@artechsoft.ro>
2013-04-08 14:05:36 +0200

    first

---
+++ VideoSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoSwitcher
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		MediaElement active;
+		MediaElement[] players;
+		Thread thr;
+		MyPipeline pipeline; 
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			MediaElement1.Source = new Uri( @"file://C:\work\MIPCUBE\v\A5606EEBBA3251B5FF34B0E831B21791_bemyapp_copy_s3.mp4");
+			MediaElement2.Source = new Uri( @"file://C:\work\MIPCUBE\v\0EC6B6B943465CC1AB83BE6D040EF630_bemyapp_copy_s3.mp4");
+			MediaElement3.Source = new Uri( @"file://C:\work\MIPCUBE\v\5943E07C5CE05CEDC9BFE476425B7004_bemyapp_copy_s3.mp4");
+			MediaElement4.Source = new Uri( @"file://C:\work\MIPCUBE\v\9C7DA9935B54648C49BB4D60C5B3E555_bemyapp_copy_s3.mp4");
+			MediaElement5.Source = new Uri( @"file://C:\work\MIPCUBE\v\313146D7BE7278CE151AF53EE2934746_bemyapp_copy_s3.mp4");
+
+			players = new MediaElement[] { MediaElement1, MediaElement2, MediaElement3, MediaElement4, MediaElement5 };
+
+			thr = new Thread( Pipeline);
+			thr.Start();
+		}
+
+		public void Pipeline()
+		{
+			pipeline=new MyPipeline(this);
+            if (!pipeline.LoopFrames())
+                Trace.WriteLine("Failed to initialize or stream data");
+            pipeline.Dispose();
+		}
+
+		void MediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+		{
+			Trace.WriteLine( "Loaded movie");
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchVideo( 0);
+		}
+
+		private void Button2_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchVideo( 1);
+		}
+
+		private void Button3_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchVideo( 2);
+		}
+
+		private void Button4_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchVideo( 3);
+		}
+
+		private void Button5_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchVideo( 4);
+		}
+
+		private void SwitchVideo( int id)
+		{
+			Dispatcher.Invoke( (Action)(() => {
+				if( active == null)
+				{
+					// play all
+					foreach( var p in players)
+					{
+						p.IsMuted = true;
+						p.Play();
+					}
+				}
+				if( active == players[id])
+					return;
+
+				Trace.WriteLine( "Switched to position " + id);
+
+				active = players[id];
+
+				//active.Position = pos;
+				active.Visibility = System.Windows.Visibility.Visible;
+				//active.Play();
+				active.IsMuted = false;
+
+				foreach( var p in players)
+					if( p != active)
+					{
+						//p.Pause();
+						p.IsMuted = true;
+						p.Visibility = System.Windows.Visibility.Hidden;
+					}
+			}));
+		}
+
+		public void HandPosition(float x, float y)
+		{
+			
+		}
+
+		public void FacePosition(float x, float y)
+		{
+			int video = (int)(x/128);
+			if( video < 0)
+				video = 0;
+			if( video > 4)
+				video = 4;
+			SwitchVideo( video);
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			foreach( var p in players)
+			{
+				p.Stop();
+			}
+			pipeline.shouldQuit = true;
+			Thread.Sleep(1000);
+			thr.Abort();
+		}
+	}
+}
---
+++ VideoSwitcher/MyPipeline.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace VideoSwitcher
+{
+	class MyPipeline: UtilMPipeline {
+        protected int  nframes;
+        protected bool device_lost;
+		private MainWindow window;
+		PXCMFaceAnalysis faceAnalysis;
+		PXCMFaceAnalysis.Detection detector;
+		public bool shouldQuit;
+
+        public MyPipeline( MainWindow w):base() {
+            //EnableGesture();
+			EnableFaceLocation();
+            nframes=0;
+            device_lost = false;
+			window = w;
+	    }
+
+	    public override void OnGesture(ref PXCMGesture.Gesture data) {
+		    if (data.active) Trace.WriteLine("OnGesture("+data.label+")");
+	    }
+        public override bool OnDisconnect()
+        {
+            if (!device_lost) Trace.WriteLine("Device disconnected");
+            device_lost = true;
+            return base.OnDisconnect();
+        }
+        public override void OnReconnect()
+        {
+            Trace.WriteLine("Device reconnected");
+            device_lost = false;
+        }
+	    public override bool OnNewFrame() 
+		{
+			if( faceAnalysis == null)
+			{
+				faceAnalysis = QueryFace();
+				detector = (PXCMFaceAnalysis.Detection)faceAnalysis.DynamicCast( PXCMFaceAnalysis.Detection.CUID);
+			}
+
+			int fid;
+			ulong timeStamp;
+			if( faceAnalysis.QueryFace( 0, out fid, out timeStamp) >= pxcmStatus.PXCM_STATUS_NO_ERROR)
+			{
+				PXCMFaceAnalysis.Detection.Data data;
+				detector.QueryData( fid, out data);
+			//	Trace.WriteLine( data.rectangle.x+data.rectangle.w/2);
+				window.FacePosition( data.rectangle.x+data.rectangle.w/2, data.rectangle.y+data.rectangle.h/2);
+			}
+			return !shouldQuit;
+
+			//PXCMGesture gesture = QueryGesture();
+			//PXCMGesture.GeoNode ndata;
+			//pxcmStatus sts = gesture.QueryNodeData(0, PXCMGesture.GeoNode.Label.LABEL_BODY_HAND_PRIMARY, out ndata);
+			//if (sts >= pxcmStatus.PXCM_STATUS_NO_ERROR)
+			//{
+			//	window.HandPosition( ndata.positionImage.x, ndata.positionImage.y);
+			//	Trace.WriteLine("node HAND_MIDDLE (" + ndata.positionImage.x +"," +ndata.positionImage.y+")");
+			//}
+			//return (++nframes<50000);
+	    }
+    };
+}
